e25436e7079a77b35e6f79710e10a8afd7aff46e
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2024-06-05 17:12:02 +0700

    feat: update api service

--- app/src/main/java/com/amoure/amoure/data/retrofit/ApiService.kt
+++ app/src/main/java/com/amoure/amoure/data/retrofit/ApiService.kt
@@ -1,14 +1,19 @@
 package com.amoure.amoure.data.retrofit
 
+import com.amoure.amoure.data.response.IdResponse
+import com.amoure.amoure.data.response.InitialResponse
 import com.amoure.amoure.data.response.LoginResponse
 import com.amoure.amoure.data.response.ProductsResponse
 import com.amoure.amoure.data.response.RegisterResponse
 import retrofit2.Call
+import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @FormUrlEncoded
@@ -17,20 +22,53 @@ interface ApiService {
         @Field("email") email: String,
         @Field("password") password: String,
         @Field("userType") userType: String
-    ): Call<RegisterResponse>
+    ): Call<InitialResponse<RegisterResponse>>
 
     @FormUrlEncoded
     @POST("auth/login")
     fun login(
         @Field("email") email: String,
         @Field("password") password: String
-    ): Call<LoginResponse>
+    ): Call<InitialResponse<LoginResponse>>
 
     @GET("products")
-    fun getProducts(): Call<ProductsResponse>
+    fun getProducts(): Call<InitialResponse<ProductsResponse>>
 
     @GET("products/{productId}")
     fun getProductById(
         @Path("productId") id: String
-    ): Call<ProductsResponse>
+    ): Call<InitialResponse<ProductsResponse>>
+
+    // TODO: Change to CartResponse
+    @GET("/carts/{userId}")
+    fun getUserCart(
+        @Path("userId") id: String
+    ): Call<InitialResponse<ProductsResponse>>
+
+    @PUT("/carts/{userId}")
+    fun putFromCart(
+        @Path("userId") id: String,
+        @Field("delivery") delivery: String,
+        @Field("deliveryPrice") deliveryPrice: Int,
+        @Field("cardNumber") cardNumber: String,
+        @Field("cardCVV") cardCVV: String,
+        @Field("cardExpiry") cardExpiry: String,
+    ): Call<InitialResponse<IdResponse>>
+
+    @POST("/carts/{userId}/{productId}")
+    fun postToCart(
+        @Path("userId") userId: String,
+        @Path("productId") productId: String
+    ): Call<InitialResponse<IdResponse>>
+
+    @DELETE("/carts/{userId}/{productId}")
+    fun deleteFromCart(
+        @Path("userId") userId: String,
+        @Path("productId") productId: String
+    ): Call<InitialResponse<IdResponse>>
+
+    @GET("products/search")
+    fun getSearch(
+        @Query("name") name: String
+    ): Call<InitialResponse<ProductsResponse>>
 }
